b822612673616728270bfd5dc34e21ecc588c118
Anthony <tonesto7@gmail.com>
2016-07-28 13:04:54 -0400

    Initial commit of nest location watchdog

---
+++ smartapps/tonesto7/nest-location-watchdog.src/nest-location-watchdog.groovy
@@ -0,0 +1,41 @@
+/**
+ *  Nest Location Watchdog
+ *
+ *  Copyright 2016 Anthony S.
+ *
+ */
+definition(
+    name: "Nest Location Watchdog",
+    namespace: "tonesto7",
+    author: "Anthony S.",
+    description: "Watches the global safety values and alerts you if they go over the threshold.",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
