67c9b6acd905215aef13e71bf47274d2d17c8eac
Anthony Santilli <Anthony@Anthonys-MBP.home.local>
2016-08-23 00:01:15 -0400

    Init commit of web dashboard. (Nothing Works Yet)

--- smartapps/tonesto7/nest-manager.src/nest-manager.groovy
+++ smartapps/tonesto7/nest-manager.src/nest-manager.groovy
@@ -127,8 +127,9 @@ mappings {
         path("/oauth/initialize") 	{action: [GET: "oauthInitUrl"]}
         path("/oauth/callback") 	{action: [GET: "callback"]}
         //Renders Json Data
-        path("/renderInstallId")  {action: [GET: "renderInstallId"]}
-        path("/renderInstallData"){action: [GET: "renderInstallData"]}
+        path("/renderInstallId")    {action: [GET: "renderInstallId"]}
+        path("/renderInstallData")  {action: [GET: "renderInstallData"]}
+        path("/dashboard")          {action: [GET: "api_dashboard"]}
         //path("/receiveEventData") {action: [POST: "receiveEventData"]}
     }
 }
@@ -251,6 +252,16 @@ def mainPage() {
                     href "nestInfoPage", title: "API | Diagnostics | Testing...", description: "Tap to view info...", image: getAppImg("api_diag_icon.png")
                 }
             }
+            section() {
+    			if (!atomicState.endpoint) {
+    				href "pageInitDashboard", title: "Nest Manager Dashboard", description: "Tap here to initialize Dashboard", image: "", required: false
+    			} else {
+    				initNestManagerEndpoint()
+    				def url = "${atomicState?.endpoint}dashboard"
+    				//log.debug "Dashboard URL: $url *** DO NOT SHARE THIS LINK WITH ANYONE ***"
+    				href "", title: "Nest Manager Dashboard", style: "external", url: url, image: getAppImg("dashboard_icon.png"), required: false
+    			}
+    		}
             section("  ") {
                 href "uninstallPage", title: "Uninstall this App", description: "Tap to Remove...", image: getAppImg("uninstall_icon.png")
             }
@@ -577,6 +588,180 @@ def getSafetyValuesDesc() {
         }
     }
     return (str != "") ? "${str}" : null
+}
+
+private pageInitDashboard() {
+	//CoRE Dashboard initialization
+	def success = initNestManagerEndpoint()
+	dynamicPage(name: "pageInitDashboard", title: "") {
+		section() {
+			if (success) {
+				paragraph "Success! Your Nest dashboard is now enabled. Tap Done to continue", required: false
+			}
+		}
+	}
+}
+
+private initNestManagerEndpoint() {
+    if (!atomicState?.endpoint) {
+		try {
+			def accessToken = atomicState?.accessToken
+			if (accessToken) {
+				atomicState?.endpoint = apiServerUrl("/api/token/${accessToken}/smartapps/installations/${app.id}/")
+			}
+		} catch(e) {
+			atomicState?.endpoint = null
+		}
+	}
+	return atomicState?.endpoint
+}
+
+def api_dashboard() {
+    def htmlData = """
+        <!DOCTYPE html>
+            <head>
+    		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    		<title>Fixed Width 2 Blue</title>
+    		<style type="text/css">
+    			html, #page { padding:0; margin:0;}
+    			body { margin:0; padding:0; width:100%; color:#959595; font:normal 12px/2.0em Sans-Serif;}
+    			h1, h2, h3, h4, h5, h6 {color:darkblue;}
+    			#page { background:#eee;}
+    			#header, #footer, #top-nav, #content, #content #contentbar, #content #sidebar { margin:0; padding:0;}
+
+    			/* Logo */
+    			#logo { padding:10px; width:auto; float:left;}
+    			#logo h1 a, h1 a:hover { color:darkblue; text-decoration:none;}
+    			#logo h1 span { color:#d3d3f9;}
+
+    			/* Header */
+    			#header { background:#00a1db; }
+    			#header-inner { margin:0 auto; padding:10px; width:970px;background:#fff;}
+
+    			/* Feature */
+    			.feature { background:#eee;padding:0;}
+    			.feature-inner { margin:auto;padding:10px;width:970px;background:blue; }
+    			.feature-inner h1 {color:#d3d3f9;font-size:32px;}
+
+    			/* Menu */
+    			#top-nav { margin:0 auto; padding:0px 0 0; height:37px; float:right;}
+    			#top-nav ul { list-style:none; padding:0; height:37px; float:left;}
+    			#top-nav ul li { margin:0; padding:0 0 0 8px; float:left;}
+    			#top-nav ul li a { display:block; margin:0; padding:8px 20px; color:blue; text-decoration:none;}
+    			#top-nav ul li.active a, #top-nav ul li a:hover { color:#d3d3f9;}
+
+    			/* Content */
+    			#content-inner { margin:0 auto; padding:10px; width:970px;background:#fff;}
+    			#content #contentbar { margin:0; padding:0; float:right; width:760px;}
+    			#content #contentbar .article { margin:0 0 24px; padding:0 20px 0 15px; }
+    			#content #sidebar { padding:0; float:left; width:200px;}
+    			#content #sidebar .widget { margin:0 0 12px; padding:8px 8px 8px 13px;line-height:1.4em;}
+    			#content #sidebar .widget h3 a { text-decoration:none;}
+    			#content #sidebar .widget ul { margin:0; padding:0; list-style:none; color:#959595;}
+    			#content #sidebar .widget ul li { margin:0;}
+    			#content #sidebar .widget ul li { padding:4px 0; width:185px;}
+    			#content #sidebar .widget ul li a { color:blue; text-decoration:none; margin-left:-16px; padding:4px 8px 4px 16px;}
+    			#content #sidebar .widget ul li a:hover { color:#d3d3f9; font-weight:bold; text-decoration:none;}
+
+    			/* Footerblurb */
+    			#footerblurb { background:#eee;color:blue;}
+    			#footerblurb-inner { margin:0 auto; width:970px; padding:10px;background:#d3d3f9;border-bottom-right-radius:15px;border-bottom-left-radius:15px;}
+    			#footerblurb .column { margin:0; text-align:justify; float:left;width:250px;padding:0 24px;}
+
+    			/* Footer */
+    			#footer { background:#eee;}
+    			#footer-inner { margin:auto; text-align:center; padding:12px; width:970px;}
+    			#footer a {color:blue;text-decoration:none;}
+
+    			/* Clear both sides to assist with div alignment  */
+    			.clr { clear:both; padding:0; margin:0; width:100%; font-size:0px; line-height:0px;}
+    		</style>
+    		<script type="text/javascript">
+
+    		</script>
+    	</head>
+    	<body>
+    		<div id="page">
+    			<header id="header">
+    				<div id="header-inner">
+    					<div id="logo">
+    						<h1><a href=""https://rawgit.com/tonesto7/nest-manager/${gitBranch()}/Images/App/nest_manager.png"></h1>
+    					</div>
+    					<div id="top-nav">
+    						<ul>
+    						<li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+    						<li><a href="#">FAQ</a></li>
+    						<li><a href="#">Help</a></li>
+    						</ul>
+    					</div>
+    					<div class="clr"></div>
+    				</div>
+    			</header>
+    			<div class="feature">
+    				<div class="feature-inner">
+    				<h1>Nest Manager Dashboard</h1>
+    				</div>
+    			</div>
+
+
+    			<div id="content">
+    				<div id="content-inner">
+
+    					<main id="contentbar">
+    						<div class="article">
+    							<p><script>generateText(12)</script></p>
+    						</div>
+    					</main>
+
+    					<nav id="sidebar">
+    						<div class="widget">
+    							<h3>Left heading</h3>
+    							<ul>
+    							<li><a href="#">Link 1</a></li>
+    							<li><a href="#">Link 2</a></li>
+    							<li><a href="#">Link 3</a></li>
+    							<li><a href="#">Link 4</a></li>
+    							<li><a href="#">Link 5</a></li>
+    							</ul>
+    						</div>
+    					</nav>
+
+    					<div class="clr"></div>
+    				</div>
+    			</div>
+
+    			<div id="footerblurb">
+    				<div id="footerblurb-inner">
+
+    					<div class="column">
+    						<h2><span>Heading</span></h2>
+    						<p><script>generateText(2)</script></p>
+    					</div>
+    					<div class="column">
+    						<h2><span>Heading</span></h2>
+    						<p><script>generateText(2)</script></p>
+    					</div>
+    					<div class="column">
+    						<h2><span>Heading</span></h2>
+    						<p><script>generateText(2)</script></p>
+    					</div>
+
+    					<div class="clr"></div>
+    				</div>
+    			</div>
+    			<footer id="footer">
+    				<div id="footer-inner">
+    					<p>&copy; Copyright <a href="#">Dashboard Link</a> &#124; <a href="#"SmartApp Version: v${appVersion()}</a></p>
+    					<div class="clr"></div>
+    				</div>
+    			</footer>
+    		</div>
+    	</body>
+        </html>
+
+    """
+	render contentType: "text/html", data: htmlData
 }
 
 def setMyLockId(val) {
