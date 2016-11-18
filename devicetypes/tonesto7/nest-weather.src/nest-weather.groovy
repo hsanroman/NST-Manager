9f284adba16061898c69f17ef8d04d3819da7df4
imnotbob <Imnot_bob@hotmail.com>
2016-11-18 10:30:21 -0500

    update healthcheck timeout

--- devicetypes/tonesto7/nest-weather.src/nest-weather.groovy
+++ devicetypes/tonesto7/nest-weather.src/nest-weather.groovy
@@ -106,7 +106,7 @@ void installed() {
 
 void verifyHC() {
 	def val = device.currentValue("checkInterval")
-	def timeOut = state?.hcTimeout ?: 35
+	def timeOut = state?.hcTimeout ?: 60
 	if(!val || val.toInteger() != timeOut) {
 		Logger("verifyHC: Updating Device Health Check Interval to $timeOut")
 		sendEvent(name: "checkInterval", value: 60 * timeOut.toInteger(), data: [protocol: "cloud"], displayed: false)
