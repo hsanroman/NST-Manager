f49961d7fb0e9f324990b991092e3d2d43fe330c
tonesto7 <tonesto7@gmail.com>
2016-11-28 11:19:50 -0500

    Updated weather to restrict the new icon size

--- devicetypes/tonesto7/nest-weather.src/nest-weather.groovy
+++ devicetypes/tonesto7/nest-weather.src/nest-weather.groovy
@@ -1020,12 +1020,12 @@ def getSunriseSunset() {
 
 def forecastDay(day) {
 	def dayName = "<b>${state.curForecast.forecast.txt_forecast.forecastday[day].title} </b><br>"
-	def forecastImageLink = "<a class=\"${day}-modal\"><img src=\"${getWeatherImg(state.curForecast.forecast.txt_forecast.forecastday[day].icon_url)}\"></a><br>"
+	def forecastImageLink = """<a class=\"${day}-modal\"><img src="${getWeatherImg(state.curForecast.forecast.txt_forecast.forecastday[day].icon_url)}" style="width:64px;height:64px;"></a><br>"""
 	def forecastTxt = ""
 
 	def modalHead = "<script> \$('.${day}-modal').click(function(){vex.dialog.alert({unsafeMessage: ' "
 	def modalTitle = " <h2>${state.curForecast.forecast.txt_forecast.forecastday[day].title}</h2>"
- 	def forecastImage = "<div class=\"centerText\"><img src=\"${getWeatherImg(state.curForecast.forecast.txt_forecast.forecastday[day].icon_url)}\"></div>"
+ 	def forecastImage = """<div class=\"centerText\"><img src="${getWeatherImg(state.curForecast.forecast.txt_forecast.forecastday[day].icon_url)}" style="width:64px;height:64px;"></div>"""
 	if ( wantMetric() ) {
 		forecastTxt = "<p>${state.curForecast.forecast.txt_forecast.forecastday[day].fcttext_metric}</p>"
 	} else {
