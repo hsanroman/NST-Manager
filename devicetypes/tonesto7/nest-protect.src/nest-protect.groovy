fd6acb9d0cc097f2e7f1fd765d4b6f37581c1411
imnotbob <Imnot_bob@hotmail.com>
2016-12-10 18:00:20 -0500

    update comparison (#128)

--- devicetypes/tonesto7/nest-protect.src/nest-protect.groovy
+++ devicetypes/tonesto7/nest-protect.src/nest-protect.groovy
@@ -167,7 +167,7 @@ void installed() {
 void verifyHC() {
 	def val = device.currentValue("checkInterval")
 	def timeOut = state?.hcTimeout ?: 35
-	if(!val || val.toInteger() != timeOut) {
+	if(!val || val.toInteger() != (timeOut.toInteger() * 60)) {
 		Logger("verifyHC: Updating Device Health Check Interval to $timeOut")
 		sendEvent(name: "checkInterval", value: 60 * timeOut.toInteger(), data: [protocol: "cloud"], displayed: false)
 	}
