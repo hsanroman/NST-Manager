9ae8d049a199d95bbe1b7013701b904cf7a604a0
tonesto7 <tonesto7@gmail.com>
2017-01-05 14:10:59 -0500

    Updated device headers to be consistent and changed to 2017

--- devicetypes/tonesto7/nest-thermostat.src/nest-thermostat.groovy
+++ devicetypes/tonesto7/nest-thermostat.src/nest-thermostat.groovy
@@ -1,31 +1,19 @@
 /**
  *  Nest Thermostat
  *	Author: Anthony S. (@tonesto7)
- *	Contributor: Ben W. (@desertBlade) & Eric S. (@E_Sch)
+ *	Co-Author: Eric S. (@E_Sch)
+ *	Contributor: Ben W. (@desertBlade)
  *  Graphing Modeled on code from Andreas Amann (@ahndee)
  *
- * Based off of the EcoBee thermostat under Templates in the IDE
- * Copyright (C) 2016 Anthony S., Ben W.
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following
- * conditions: The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Modelled after the EcoBee thermostat under Templates in the IDE
+ * Copyright (C) 2017 Anthony S.
+ * Licensing Info: Located at https://raw.githubusercontent.com/tonesto7/nest-manager/master/LICENSE.md
  */
 
 import java.text.SimpleDateFormat
 import groovy.time.*
 
-def devVer() { return "4.3.0"}
+def devVer() { return "4.4.0"}
 
 // for the UI
 metadata {
--- devicetypes/tonesto7/nest-weather.src/nest-weather.groovy
+++ devicetypes/tonesto7/nest-weather.src/nest-weather.groovy
@@ -1,24 +1,11 @@
 /**
  *  Nest Weather
  *  Author: Anthony S. (@tonesto7)
- *  Author: Ben W. (@desertBlade)  Eric S. (@E_sch)
+ *  Co-Authors: Ben W. (@desertBlade)  Eric S. (@E_sch)
  *  Graphing Modeled on code from Andreas Amann (@ahndee)
  *
- * Copyright (C) 2016 Anthony S., Ben W.
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following
- * conditions: The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *	Copyright (C) 2017 Anthony S., Ben W.
+ * 	Licensing Info: Located at https://raw.githubusercontent.com/tonesto7/nest-manager/master/LICENSE.md
  */
 
 import java.text.SimpleDateFormat
